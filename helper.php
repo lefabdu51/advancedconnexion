c398b4b57f5e71427559501bc174d98304529f06
lefabdu51 <61248251+lefabdu51@users.noreply.github.com>
2020-02-22 13:43:51 +0100

    Create helper.php

---
+++ helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package   Advanced Connexion Module for Joomla!
+ * @author    Informaticien51.fr https://informaticien51.fr
+ * @copyright Copyright (C) 2020 Informaticien51.fr, All rights reserved.
+ * @license   GNU/GPLv3 or later; https://www.gnu.org/licenses/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+class ModAdvancedConnexionHelper
+{
+/** Connexion d'un utilisateur
+* Récupère le module de menu associé 
+* $params: paramètres du module de connexion
+* $menuparams: paramètres du module de menu associé
+*/
+}
