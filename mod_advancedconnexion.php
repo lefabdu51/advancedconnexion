95ad0b5e8c13b6afc73dc07a4162edce93259554
lefabdu51 <61248251+lefabdu51@users.noreply.github.com>
2020-02-22 13:54:09 +0100

    Create mod_advancedconnexion.php

---
+++ mod_advancedconnexion.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*
+*/
+// No direct access
+defined('_JEXEC') or die;
+// Load the helper file via Joomla's Autoload feature
+JLoader::register('ModAdvancedConnexionHelper', __DIR__ . '/helper.php');
