bf795eac70db4d201baf3373b4e71440914fd23a
lefabdu51 <61248251+lefabdu51@users.noreply.github.com>
2020-02-22 14:18:19 +0100

    Update install.mysql.utf8.sql

--- sql/mysql/install.mysql.utf8.sql
+++ sql/mysql/install.mysql.utf8.sql
@@ -1,1 +1,15 @@
-
+CREATE TABLE IF NOT EXISTS `#__mod_advancedconnexion` (
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`menu_link` text NOT NULL,
+	`url` varchar(25) NOT NULL, 
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+ 
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
+INSERT INTO `#__mod_advancedconnexion` (`menu_link`, `url`) VALUES ('', '');
